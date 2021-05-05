703f64a2da73ddd9d8d333475a446bd042bbeb7e
jos3duardo <jos3duardolopes@gmail.com>
2021-05-04 23:41:32 -0400

    create routes for api resources

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Http\Controllers\v1\ClientController;
+use App\Http\Controllers\v1\OrderController;
+use App\Http\Controllers\v1\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function (){
+    Route::apiResource('clients', ClientController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('orders', OrderController::class);
 });
