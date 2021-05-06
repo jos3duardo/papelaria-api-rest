0f03f411c5cb00588d49f13d1c8f0bcaff866d53
jos3duardo <jos3duardolopes@gmail.com>
2021-05-05 22:20:49 -0400

    crud of clients

--- app/Http/Controllers/v1/ClientController.php
+++ app/Http/Controllers/v1/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ClientRequest;
+use App\Http\Requests\ClientUpdateRequest;
 use App\Http\Resources\ClientResource;
 use App\Models\Client;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,29 +21,40 @@ class ClientController extends Controller
     public function store(ClientRequest $request)
     {
         try {
-            $client = Client::firstOrCreate([
-                $request->all()
-            ]);
-            dd($client);
+            $client = Client::create($request->all());
             return response()->json(new ClientResource($client), Response::HTTP_OK);
         }catch (\Exception $err){
             return response()->json($err->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
     }
 
     public function show(Client $client)
     {
-        //
+        try {
+            return response()->json(new ClientResource($client), Response::HTTP_OK);
+        }catch (\Exception $err){
+            return response()->json($err->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public function update(ClientRequest $request, Client $client)
+    public function update(ClientUpdateRequest $request, Client $client)
     {
-        //
+        try {
+            $client->fill($request->all());
+            $client->save();
+            return response()->json(new ClientResource($client), Response::HTTP_OK);
+        }catch (\Exception $err){
+            return response()->json($err->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function destroy(Client $client)
     {
-        //
+        try {
+            $client->delete();
+            return response()->json(new ClientResource($client), Response::HTTP_OK);
+        }catch (\Exception $err){
+            return response()->json($err->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -8,6 +8,17 @@ class ClientResource extends JsonResource
 {
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            "phone" => $this->phone,
+            "date_birth" => $this->date_birth,
+            "address" => $this->address,
+            "complement" => $this->complement,
+            "neighborhood" => $this->neighborhood,
+            "zip_code" => $this->zip_code,
+            "date_registration" => $this->date_registration,
+        ];
     }
 }
