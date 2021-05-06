239e86c6d399e83be2d350f5fd1180ea333b059e
jos3duardo <jos3duardolopes@gmail.com>
2021-05-05 23:45:05 -0400

    file routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\v1\ClientController;
 use App\Http\Controllers\v1\OrderController;
 use App\Http\Controllers\v1\ProductController;
+use App\Http\Controllers\v1\ProductTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +14,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function (){
     Route::apiResource('clients', ClientController::class);
     Route::apiResource('products', ProductController::class);
+    Route::apiResource('products-types', ProductTypeController::class);
     Route::apiResource('orders', OrderController::class);
 });
