532c4be3b339b7375990dd68a829c596d1255e40
jos3duardo <jos3duardolopes@gmail.com>
2021-05-06 20:45:39 -0400

    refactor migration

--- database/migrations/2021_05_05_012726_create_orders_table.php
+++ database/migrations/2021_05_05_012726_create_orders_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateOrdersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
@@ -24,11 +19,6 @@ class CreateOrdersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('orders');
